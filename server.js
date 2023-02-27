5821a4d10050d9760b3e4ed134b4994ef5dd324e
StevenYAMBOS <stevenyambos@gmail.com>
2023-02-27 22:59:19 +0100

    Ajout des Views

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 // On utilise le module Express
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
+dotenv.config({path: 'config.env'})
 const PORT = process.env.PORT || 8080
 
+app.use(morgan('tiny'));
+
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
+
 app.get('/', (req, res) => {
-    res.send('Application CRUD');
+    // Rendre un fichier HTML
+    res.render('index.ejs')
 })
 
 app.listen(3000, () => {
